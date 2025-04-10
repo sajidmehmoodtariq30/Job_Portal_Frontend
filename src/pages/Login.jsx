cf607fd48e246574ac04dd50b508eda9a59ec855
Muhammad Haseeb IQBAL <155799767+HaseebIqbal1199@users.noreply.github.com>
2025-04-10 20:42:29 +0500

    Add UI components and update dependencies for improved styling and functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { Button } from "@/components/UI/button"
+import { Input } from "@/components/UI/input"
+import { Label } from "@/components/UI/label"
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
@@ -14,7 +17,18 @@ const LoginPage = () => {
     <div className="w-full h-full flex items-center justify-center">
       <div className="w-[70%] h-[80%] bg-amber-900 rounded-lg flex overflow-hidden">
         <div className="w-[50%] h-full bg-amber-500"></div>
-        <div className="w-[50%] h-full bg-blue-500"></div>
+        <div className="w-[50%] h-full bg-blue-500">
+          <h1>Authentication</h1>
+          <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div>
+    <div className="grid w-full max-w-sm items-center gap-1.5">
+      <Label htmlFor="email">Email</Label>
+      <Input type="email" id="email" placeholder="Email" />
+    </div>
+    <Button>Button</Button>
+        </div>
       </div>
     </div>
   );
